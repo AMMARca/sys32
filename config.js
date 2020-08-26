a52f9d7df03342b4dc136d38edb50b52f919a7e6
AMMARca <68115629+AMMARca@users.noreply.github.com>
2020-08-27 01:05:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzEyMDk0NzAwNjA5MzM5NDQz.XvsbNA.QIp7n3RQdJRND7lIMoID-uP5bHY", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+  devs: ["541375390602493982"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
